6aae8f2c9ca5934abfbb4d2939d960e4b1523cc2
Elano N. Caitano <nunes.elano14@gmail.com>
2023-01-16 22:20:27 -0300

    Conexão com banco usando Prisma

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get("/", () => {
-  return "Hello World";
+app.get("/", async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        contains: "Star Wars"
+      }
+    }
+  });
+
+  return habits;
 });
 
 app.listen({port: 3000}).then(()=> console.log("Listening on port 3000"));
